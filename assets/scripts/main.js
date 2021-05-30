24a053d27ff50aa960837bb8ade6ac117c4b18c2
blackc0mb <santmd77@gmail.com>
2021-05-29 22:25:45 -0500

    Replace switch sentence (language) with a function

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,23 +4,7 @@ link_about = document.getElementById('link-about'),
 about_divider = document.getElementById("about-divider")
 
 //Toggle language declarations
-let link_toggle_lang = document.getElementById("link-toggle-lang")
-const ENGLISH = "en",
-ESPANOL = "es"
-
-const label_en = {
-   l_blog: "Blog & Notes",
-   l_password: "Password Generator",
-   l_about: "About",
-   l_about_content: "Hello 👋, my name is Tonatiuh Morales and currently I work 💻 mainly with SAP platforms for several years, also I'm a developer in my free time (at least I try it). </br>Thanks for visiting! 🙌"
-}
-
-const label_es = {
-   l_blog: "Notas / Blog",
-   l_password: "Generador de contraseñas",
-   l_about: "Sobre mi",
-   l_about_content: "Hola 👋, mi nombre es Tonatiuh Morales y actualmente trabajo 💻 principalmente con plataformas SAP desde hace ya varios años, también le hago un poco al desarrollo de software en mi tiempo libre (al menos lo intento).</br>Gracias por visitar! 🙌"
-}
+let link_toggle_lang = document.getElementById("l_link_toggle_lang")
 
 //Show-hide de about section
 show_about.classList.add('visuallyhidden')
@@ -45,27 +29,36 @@ link_about.addEventListener('click', function () {
    }
 }, false)
 
+//Switch English/Spanish
 link_toggle_lang.addEventListener('click', function () {
    let lang = {}
    let myLabels = document.querySelectorAll("[id^='l_']")
-   let current_lang = ESPANOL
 
-   current_lang = link_toggle_lang.innerHTML.includes("English") === true ? ENGLISH : ESPANOL
-
-   switch (current_lang) {
-      case ESPANOL:
-         link_toggle_lang.innerHTML = 'English'
-         lang = label_es
-         break
-
-      case ENGLISH:
-         link_toggle_lang.innerHTML = 'Español'
-         lang = label_en
-         break
-   }
+   lang = retLanguage(link_toggle_lang.innerHTML)
 
    for (let i = 0; i < myLabels.length; i++) {
       myLabels[i].innerHTML = lang[myLabels[i].id]
    }
 
 })
+
+function retLanguage(lang) {
+   const language = {
+      'English': {
+         l_blog: "Blog & Notes",
+         l_password: "Password Generator",
+         l_about: "About",
+         l_about_content: "Hello 👋, my name is Tonatiuh Morales and currently I work 💻 mainly with SAP platforms for several years, also I'm a developer in my free time (at least I try it). </br>Thanks for visiting! 🙌",
+         l_link_toggle_lang: "Español"
+      },
+      'Español': {
+         l_blog: "Notas / Blog",
+         l_password: "Generador de contraseñas",
+         l_about: "Sobre mi",
+         l_about_content: "Hola 👋, mi nombre es Tonatiuh Morales y actualmente trabajo 💻 principalmente con plataformas SAP desde hace ya varios años, también le hago un poco al desarrollo de software en mi tiempo libre (al menos lo intento).</br>Gracias por visitar! 🙌",
+         l_link_toggle_lang: "English"
+      } 
+   }
+
+   return language[lang]
+}
